0e998b53738a9774e8183c53c262d3bb372f5efa
frenkiart <frenki_art@mail.ru>
2021-03-22 17:45:29 +0300

    Обновил свой шаплон, добавил несколько элементов и из последней работы

--- src/index.js
+++ src/index.js
@@ -19,21 +19,24 @@ import './views/components/form/form.scss';
 import './views/components/header/header.scss';
 import './views/components/links/link.scss';
 import './views/components/menu/menu.scss';
+import './views/components/tabs/tabs.scss';
+import './views/components/tabs/tabs.js';
 import './views/components/pagination/pagination.scss';
+import './views/components/popup/popup.scss';
+import {myPopup} from './views/components/popup/popup.js';
 import './views/mixins/mixins.scss';
 
 // Страницы
 // import './views/components/pages/index.scss';
 
-/**
- * Эта функция выводит ширину вьюпорта тогда,
- * когда происходит изменение размера окна
- * @date 2021-02-09
- * @return {void}
- */
+/* Про Попапы */
+if (document.querySelector('[data-popup]')) {
+  myPopup(document.querySelectorAll('[data-popup]'));
+}
 
-window.onresize = () => {
-  setTimeout(() => {
-    console.log(window.innerWidth);
-  }, 1);
-};
+const customJsScript = document.createElement('script');
+customJsScript.type = 'text/javascript';
+customJsScript.defer = true;
+customJsScript.src = './assets/js/custom-js.js';
+
+document.body.appendChild(customJsScript);
